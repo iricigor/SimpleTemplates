f5e599f340a96a11fc25b2f21d1c0c431e2b55cd
Igor Irić <iricigor@gmail.com>
2022-10-18 21:32:03 +0200

    added comment

--- TestModule.ps1
+++ TestModule.ps1
@@ -1,3 +1,5 @@
+# basic verification of module functionality
+# used after the module is built
 Import-Module ./bin/Debug/netstandard2.0/SimpleTemplates.dll -Force
 Get-Command -Module SimpleTemplates
 New-STFunction 12
