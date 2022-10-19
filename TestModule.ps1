efe64d2cf713a973f3904f1b4eeb9baf6537f83a
Igor Iric <iiric@microsoft.com>
2022-10-19 18:14:20 +0200

    added skeleton for new-function command

--- TestModule.ps1
+++ TestModule.ps1
@@ -2,4 +2,18 @@
 # used after the module is built
 Import-Module ./bin/Debug/netstandard2.0/SimpleTemplates.dll -Force
 Get-Command -Module SimpleTemplates
-New-STFunction 12
+
+# Note: Do not remove those tests, add them to the Pester later!
+
+
+# Basic functionality test
+$FunctionName = 'Get-Greeting'
+$FunctionFile = "$FunctionName.ps1"
+
+New-STFunction $FunctionName
+if (Get-Item $FunctionFile) {
+    Write-Host "Function file created: $FunctionFile"
+    Remove-Item $FunctionFile
+} else {
+    Write-Host "Function file not created: $FunctionFile"
+}
