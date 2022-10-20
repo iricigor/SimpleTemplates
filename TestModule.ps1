bc67bde82ffa66bbe324e7dfbebd35fffc0aa07f
Igor Iric <iiric@microsoft.com>
2022-10-20 11:02:38 +0200

    reorged tests

--- TestModule.ps1
+++ TestModule.ps1
@@ -11,12 +11,20 @@ $FunctionName = 'Get-Greeting'
 $FunctionFile = "$FunctionName.ps1"
 
 New-STFunction $FunctionName -Verbose
-if (Get-Item $FunctionFile) {
-    Write-Host "Function file created: $FunctionFile"
-    Write-Host "`nFunction file content:"
-    Get-Content $FunctionFile
-    Write-Host "`nRemoving function file..."
-    Remove-Item $FunctionFile
-} else {
-    Write-Host "Function file not created: $FunctionFile"
+if (!(Get-Item $FunctionFile)) {
+    Write-Error "Function file not created: $FunctionFile"
 }
+Write-Host "Function file created: $FunctionFile"
+
+Write-Host "`nFunction file content:"
+Get-Content $FunctionFile
+
+Write-Host "`nExecuting function file..."
+. ./$FunctionFile
+
+Write-Host "It should have generate a function $FunctionName"
+Get-Command $FunctionName
+Get-Help $FunctionName
+
+Write-Host "`nRemoving function file..."
+Remove-Item $FunctionFile
